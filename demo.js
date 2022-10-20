c9714f98175ee3e8971307c7515af9b6445e3ec2
Tapash Dutta <tapash.dutta@indusnet.co.in>
2022-10-20 15:23:57 +0530

    modification

--- demo.js
+++ demo.js
@@ -1,9 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
 
 exports.handler = async (event, context) => {
-    console.log(event);
+    console.log('event', event);
+    console.log('Context', context);
     const newToken = uuidv4();
-    console.log(newToken);
+    console.log('uuid token', newToken);
     return {
         statusCode: 200,
         message: 'Successful'
